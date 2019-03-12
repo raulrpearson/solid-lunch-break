d74fa6fcde3cfa87d81d9758025dbb5465b79cd9
Raúl R. Pearson <raulrpearson@protonmail.com>
2019-03-12 11:01:15 +0700

    Add log in and out functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
 $('#logout').hide();
+const popupUri = 'popup.html';
+$('#login button').click(() => solid.auth.popupLogin({ popupUri }));
+solid.auth.trackSession(session => {
+  const loggedIn = !!session;
+  $('#login').toggle(!loggedIn);
+  $('#logout').toggle(loggedIn);
+  $('#user').text(session && session.webId);
+});
+$('#logout button').click(() => solid.auth.logout());
